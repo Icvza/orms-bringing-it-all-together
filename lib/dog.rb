b8c832423729fb52c322c7066e7b343b537bf352
Isaac <deisaacalejandro@gmail.com>
2021-01-24 18:28:31 -0500

    commit tree 67d5a1135898233584e78ec0ae00b2566b60c0c2

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,11 @@ class Dog
         # binding.pry 
     end
 
+    def update
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
     def save
         sql = <<-SQL
         INSERT INTO dogs (name, breed) 
@@ -42,12 +47,38 @@ class Dog
       self
     end
 
-    def update
-    end
+    def self.create(name:, breed:)
+        dog = Dog.new(name:name, breed:breed)
+        dog.save 
+        dog
+    end 
 
     def self.find_by_name(name)
         sql = "SELECT * FROM dogs WHERE name = ?"
         result = DB[:conn].execute(sql, self.name, self.breed, self.id)
+    end
+
+    def self.find_or_create_by(name:, breed:)
+        dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+        if !dog.empty?
+            dog_data = dog[0]
+            dog = Dog.new(id:dog_data[0], name:dog_data[1], breed:dog_data[2])
+        else
+            dog = self.create(name: name, breed: breed)
+        end
+        dog
+    end
+    
+    def self.find_by_name(name)
+        sql = "SELECT * FROM dogs WHERE name = ?"
+        result = DB[:conn].execute(sql, name)[0]
+        Dog.new(id:result[0], name:result[1], breed:result[2])
+    end
+
+    def self.find_by_id(id)
+        sql = "SELECT * FROM dogs WHERE id = ?"
+        result = DB[:conn].execute(sql, id)[0]
+        Dog.new(id:result[0], name:result[1], breed:result[2])
     end
 
 
